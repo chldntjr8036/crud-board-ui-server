b819d823cd92d4682f1b4b4dde9282dae1c078ec
Choi wooseok <cwooseok60@gmail.com>
2019-12-24 17:47:45 +0900

    [issue3] Article 생성 시, dateTime 포매팅 관련 이슈
    생성 시간을 서버에서 따로 정하는게 아니라,
    프론트에서 표준 datetime 형식에 따라 String 값으로 보낸다.
    포맷 형식은 'YYYY-MM-DDTHH:MM:SS'

--- out/production/ui/js/board/board_write.js
+++ out/production/ui/js/board/board_write.js
@@ -1,11 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
+    //자리수 맞추기 formatter
+    let format = function(num) {
+        if(num/10 < 1) {
+            return String("0"+num);
+        }
+        return String(num);
+    }
+
     //글쓰기 버튼 이벤트 처리
     $('#writeButton').click(function () {
+
+        let date = new Date();
+        let dateTime = date.getFullYear()+'-'
+            +format(date.getMonth())+'-'
+            +format(date.getDay())+'T'
+            +format(date.getHours())+':'
+            +format(date.getMinutes())+':'
+            +format(date.getSeconds());
+
         let dataObj = {
             'title':$('#title').val(),
             'userName':$('#userName').val(),
             'password':$('#password').val(),
-            'contents':$('#contents').val()
+            'contents':$('#contents').val(),
+            'dateTime':dateTime
         };
         $.ajax({
             type: 'POST',
